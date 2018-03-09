445a92469815c8281ecaa81fd96be2f50724d1ab
Preston Sego <LPSego3+dev@gmail.com>
2018-03-08 20:40:46 -0500

    add get-orders to example

--- examples/cobinhood/check-order-status.ts
+++ examples/cobinhood/check-order-status.ts
@@ -6,7 +6,11 @@ dotenv.config({ path: '.env' });
 async function begin() {
   const client = new CobinhoodRestClient();
 
-  const order = await client.getOrder('37f550a202aa6a3fe120f420637c894c');
+  const orders = await client.getOpenOrders();
+
+  console.log(orders);
+
+  const order = await client.getOrder('7444b1fd-f9a2-4249-949c-d77a2e355d40');
 
   console.log(order);
 }
